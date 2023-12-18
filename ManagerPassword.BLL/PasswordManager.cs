63936e1933b29be8a973796bae07c99f7ed0f432
Alexander Chechetko <ggg.kacheck@gmail.com>
2023-12-18 22:28:39 +0600

    Добавьте файлы проекта.

---
+++ ManagerPassword.BLL/PasswordManager.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using ManagerPassword.DAL;
+
+namespace ManagerPassword.BLL
+{
+    public class PasswordManager
+    {
+        private readonly PasswordDatabase passwordDatabase;
+        private string masterPassword;
+        private const string PasswordKeySalt = "SaltForPasswordKey";
+      
+
+
+        public PasswordManager()
+        {
+            passwordDatabase = new PasswordDatabase();
+        }
+
+        public void SetMasterPassword()
+        {
+            try
+            {
+                Console.WriteLine("Введите мастер-пароль:");
+                masterPassword = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(masterPassword))
+                {
+                    Console.WriteLine("Предупреждение: Пустой пароль может быть небезопасным.");
+                }
+
+                Console.WriteLine("Мастер-пароль установлен.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при установке мастер-пароля: {ex.Message}");
+            }
+        }
+
+        public void AddPasswordEntry()
+        {
+            try
+            {
+                Console.WriteLine("Введите веб-сайт:");
+                string website = Console.ReadLine();
+
+                Console.WriteLine("Введите логин:");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Введите пароль:");
+                string password = Console.ReadLine();
+
+                // Проверки на пустые строки или недопустимые символы
+                if (string.IsNullOrWhiteSpace(website) || string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                {
+                    throw new ArgumentException("Некорректный ввод. Пожалуйста, убедитесь, что введены все данные.");
+                }
+
+                string encryptedPassword = EncryptPassword(password);
+
+                PasswordEntry entry = new PasswordEntry
+                {
+                    Website = website,
+                    Username = username,
+                    EncryptedPassword = encryptedPassword
+                };
+
+                passwordDatabase.AddPassword(entry);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при добавлении записи: {ex.Message}");
+            }
+        }
+
+        private string EncryptPassword(string password)
+        {
+            try
+            {
+                using (AesManaged aesAlg = new AesManaged())
+                {
+                    Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(masterPassword, Encoding.UTF8.GetBytes(PasswordKeySalt));
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+
+                    using (ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV))
+                    {
+                        using (MemoryStream msEncrypt = new MemoryStream())
+                        {
+                            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                                {
+                                    swEncrypt.Write(password);
+                                }
+                            }
+
+                            return Convert.ToBase64String(msEncrypt.ToArray());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при шифровании пароля: {ex.Message}");
+                return null;
+            }
+        }
+        public void ViewPasswordEntries()
+        {
+            try
+            {
+                Console.WriteLine("Список сохраненных записей:");
+                var entries = passwordDatabase.GetAllPasswords();
+                foreach (var entry in entries)
+                {
+                    Console.WriteLine($"Веб-сайт: {entry.Website,-20} Логин: {entry.Username}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при просмотре записей: {ex.Message}");
+            }
+        }
+
+        public void SearchPasswordEntries(string query)
+        {
+            try
+            {
+                var searchResults = passwordDatabase.SearchPasswords(query);
+
+                if (searchResults.Any())
+                {
+                    Console.WriteLine("Результаты поиска:");
+                    foreach (var entry in searchResults)
+                    {
+                        Console.WriteLine($"Веб-сайт: {entry.Website,-20} Логин: {entry.Username}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Записей не найдено.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при поиске записей: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ManagerPassword.DAL/PasswordDatabase.cs
@@ -0,0 +1,68 @@
+﻿using LiteDB;
+using ManagerPassword;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ManagerPassword.DAL
+{
+    public class PasswordDatabase
+    {
+        private readonly LiteDatabase db;
+        private readonly LiteCollection<PasswordEntry> passwordEntries;
+
+        public PasswordDatabase()
+        {
+            db = new LiteDatabase("PasswordManager.db");
+            passwordEntries = (LiteCollection<PasswordEntry>)db.GetCollection<PasswordEntry>("passwordEntries");
+        }
+
+        public void AddPassword(PasswordEntry entry)
+        {
+            try
+            {
+                passwordEntries.Insert(entry);
+                Console.WriteLine("Запись добавлена.");
+            }
+            catch (LiteException ex)
+            {
+                Console.WriteLine($"Произошла ошибка при добавлении записи: {ex.Message}");
+            }
+        }
+
+        public List<PasswordEntry> GetAllPasswords()
+        {
+            try
+            {
+                return passwordEntries.FindAll().ToList();
+            }
+            catch (LiteException ex)
+            {
+                Console.WriteLine($"Произошла ошибка при получении записей: {ex.Message}");
+                return new List<PasswordEntry>();
+            }
+        }
+
+        public List<PasswordEntry> SearchPasswords(string query)
+        {
+            try
+            {
+                return passwordEntries
+    .Find(x => x.Website.IndexOf(query, StringComparison.OrdinalIgnoreCase) != -1 ||
+               x.Username.IndexOf(query, StringComparison.OrdinalIgnoreCase) != -1)
+    .ToList();
+            }
+            catch (LiteException ex)
+            {
+                Console.WriteLine($"Произошла ошибка при поиске записей: {ex.Message}");
+                return new List<PasswordEntry>();
+            }
+        }
+
+        // Другие методы для работы с базой данных, если необходимо
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
